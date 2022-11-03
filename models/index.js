d59cfa2fad15935d15cd2fa3953d69bfaa6c5c2e
Matthew Sidaway <matthewsidaway@outlook.com>
2022-11-03 16:02:32 +0000

    defined associations

--- models/index.js
+++ models/index.js
@@ -5,6 +5,14 @@ const User = require('./User.model');
 /* DEFINE RELATIONSHIPS WITHIN THIS FILE
 *****************************************/
 
+/* ONE ==> MANY: USER ==> BOARD */
+User.hasMany(Board);
+Board.belongsTo(User);
+
+/* MANY ==> MANY: CHEESE ==> BOARD */
+Board.belongsToMany(Cheese, { through: 'Board_Cheese' });
+Cheese.belongsToMany(Board, { through: 'Board_Cheese' });
+
 module.exports = {
     Cheese,
     Board,
